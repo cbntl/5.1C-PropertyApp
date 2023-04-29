7940ebd4f59c5403ccecd1092dc88bc8ce57c9b5
cbntl <claire.bantilan@gmail.com>
2023-04-29 22:05:18 +1000

    Initial Commit for Property App

---
+++ app/src/main/java/com/example/sit708_task5_1property/Property.java
@@ -0,0 +1,51 @@
+package com.example.sit708_task5_1property;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Property {
+    private Integer rent,bedroomCount,carParkCount,bathroomCount;
+    private String location;
+
+
+    public Property(String location, Integer rent, Integer bedroomCount, Integer carParkCount, Integer bathroomCount) {
+        this.location = location;
+        this.rent = rent;
+        this.bedroomCount = bedroomCount;
+        this.carParkCount = carParkCount;
+        this.bathroomCount = bathroomCount;
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Integer getRent() {
+        return rent;
+    }
+    public Integer getBedroomCount() {
+        return bedroomCount;
+    }
+    public Integer getCarParkCount() {
+        return carParkCount;
+    }
+    public Integer getBathroomCount() {
+        return bathroomCount;
+    }
+
+    public static List<Property> propertyList() {
+        List<Property> propertyList = new ArrayList<>();
+        for (int i=0; i<3; i++) {
+            String location = "Albert Street";
+            Property property = new Property(location, 500+((i+1)*100),3,2,2);
+            propertyList.add(property);
+        }
+        for (int i=0; i<4; i++) {
+            String location = "Burwood Street";
+            Property property = new Property(location, 1000+((i+1)*100),3,2,2);
+            propertyList.add(property);
+        }
+        return propertyList;
+    }
+}
